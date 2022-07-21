23e62833edfe9f5465a44af0cf378c99d9a4f1c4
Joseph Freeston <snorklerjoe@gmail.com>
2022-07-20 21:43:19 -0400

    Created a Resource structure for data

--- src/api/server/resources/__init__.py
+++ src/api/server/resources/__init__.py
@@ -1,2 +1,22 @@
 """This package contains the resource classes for the api
 """
+
+from flask import request
+from flask_restful import Resource
+
+class Data(Resource):
+    """A POST-only resource for datapoints"""
+
+    DATA_TYPES = {  # TODO: Update data types for any possible datapoints
+        "temperature": float,
+        "humidity": float,
+        "pressure": float,
+        "light_intensity": float,
+        "comment": str
+    }
+    """Defines the types of parameters used to post data"""
+
+    def post(self):
+        print(f"POSTing data {request.form}.")
+        return request.form, 201
+
