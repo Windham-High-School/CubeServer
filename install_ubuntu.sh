e0a8d1c21646a91f3a1dca019330eb73e432928a
Joseph Freeston <snorklerjoe@gmail.com>
2022-10-29 15:32:47 -0400

    Install script fixes

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -13,7 +13,7 @@ sudo apt-get update
 
 # Check if interactive shell:
 INTERACTIVE=0
-[[ $- == *i* ]] && INTERACTIVE=1 && sudo apt-get install dialog
+[ -t 0 ] && INTERACTIVE=1 && sudo apt-get install dialog
 
 
 pipe_output () {
@@ -24,23 +24,30 @@ message () {
     [[ $INTERACTIVE -ne 0 ]] && dialog --backtitle "${INTERACTIVE_BACKTITLE}" --title "$1" --infobox "$2" 15 75 || echo -e "$1\n$2"
 }
 
+prepare () {    # Install Docker and Docker-Compose
+    message "Installing Dependencies" "Installing docker and docker-compose..."
+    sudo apt-get -y install curl gnupg ca-certificates lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    echo "Downloading keyring..." && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update | pipe_output "Updating Package Lists"
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose | pipe_output "Installing Docker"
+    message "Preparing docker..."
+    sudo gpasswd -a $USER docker
+}
 
-# Install Docker and Docker-Compose
-sudo apt-get -y install curl gnupg ca-certificates lsb-release | pipe_output "Installing PGP Cert Tools"
-sudo mkdir -p /etc/apt/keyrings
-echo "Downloading keyring..." && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update | pipe_output "Updating Package Lists"
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose | pipe_output "Installing Docker"
+build () {
+    docker-compose build --no-cache | pipe_output "Building With Docker-Compose"
+}
 
-docker-compose build --no-cache | pipe_output "Building With Docker-Compose"
-
-# Create system service
-message "Installing" "Creating Systemd Service..."
-cat > /etc/systemd/system/cubeserver.service << EOF
+install () {    # Create system service
+    message "Installing" "Creating Systemd Service..."
+    cat > /etc/systemd/system/cubeserver.service << EOF
 [Unit]
 Description=${SOFTWARE_NAME} WebApp, API, Backend, etc.
 PartOf=docker.service
+After=network.target
+After=network-online.target
 After=docker.service
 
 [Service]
@@ -58,9 +65,5 @@ ExecStop=$(which docker-compose) -f docker-compose.yml down
 [Install]
 WantedBy=multi-user.target
 EOF
-
-message "Installing" "Enabling & Starting Systemd Service..."
-sudo systemctl enable cubeserver.service
-sudo systemctl start cubeserver.service
-
-message "Finished!" "The ${SOFTWARE_NAME} installation is complete!\nThe server should be started and should start itself on boot."
+    sudo systemctl enable cubeserver.service"
+}
