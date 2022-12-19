53c2a3c97aa598af9f984fc86e4c14c2313dacec
Joseph Freeston <snorklerjoe@gmail.com>
2022-12-18 21:01:15 -0500

    fixed license updater

--- src/CubeServer-common/cubeserver_common/_license.py
+++ src/CubeServer-common/cubeserver_common/_license.py
@@ -1,4 +1,4 @@
-"""
+__license__ = """
 
 MIT License
 
--- update_license.sh
+++ update_license.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 # Updates the license across Python packages and such
 
-LICENSE=$(cat LICENSE)
-LICENSE_STR="__license__ = \"\"\"\n$LICENSE\n\"\"\""
-
-echo -e "\"\"\"\n" > ./src/CubeServer-common/cubeserver_common/_license.py
+echo -e "__license__ = \"\"\"\n" > ./src/CubeServer-common/cubeserver_common/_license.py
 cat LICENSE >> ./src/CubeServer-common/cubeserver_common/_license.py
 echo -e "\n\"\"\"" >> ./src/CubeServer-common/cubeserver_common/_license.py
 
