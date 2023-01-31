1ad04b2770b4b6ef99b497e70b3d7b7b00358359
Joseph Freeston <101837621+snorklerjoe@users.noreply.github.com>
2023-01-31 19:51:51 +0000

    Fixed screen suggestion workaround

--- tools/pull_release.sh
+++ tools/pull_release.sh
@@ -10,14 +10,12 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-case $(ps -o comm= -p "$PPID") in
-    sshd|*/sshd)
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
         echo -e "${RED}This is being run in an SSH session directly."
         echo -e "This is dangerous, as the build could terminate prematurely-"
         echo -e "Please run this in a screen session instead:"
         echo -e "${CYAN}screen -L bash pull_release.sh${RESTORE}"
-    ;;
-esac
+fi
 
 PREV_VER=$(cat ../version.txt)
 
