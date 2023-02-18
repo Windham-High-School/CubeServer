e2daea5541a7254172bb81ced6ac14ab3433ab35
Joseph Freeston <snorklerjoe@gmail.com>
2023-02-18 09:38:35 -0500

    Unified configure_db method for both app and noapp

--- src/CubeServer-common/cubeserver_common/__init__.py
+++ src/CubeServer-common/cubeserver_common/__init__.py
@@ -2,17 +2,21 @@
 
 import os
 from flask_pymongo import PyMongo
-from pymongo import MongoClient
 from cubeserver_common.models import PyMongoModel
 
 
-def configure_db(app):
+def configure_db(app=None):
     """Configures the database"""
     # Configure MongoDB:
-    app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] \
+    uri = 'mongodb://' + os.environ['MONGODB_USERNAME'] \
         + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] \
             + ':27017/' + os.environ['MONGODB_DATABASE'] + '?authSource=admin'
-    mongo = PyMongo(app)
+    if app is not None:
+        app.config["MONGO_URI"] = uri
+        mongo = PyMongo(app, uri=uri)
+    else:
+        mongo = PyMongo(uri=uri)
+    print(f"\n\n\tMONGO: {mongo}\n\n")
     PyMongoModel.update_mongo_client(mongo)
     # Don't let the model classes load until after the db is init'd:
     from cubeserver_common.models.config.conf import Conf
@@ -23,11 +27,3 @@ def configure_db(app):
     if Conf.retrieve_instance() is None:
         default_confset = Conf()
         default_confset.save()
-
-def configure_db_noapp():
-    """For Flask-boycotters (for CubeServer-beaconserver :D)"""
-    mongo_uri = 'mongodb://' + os.environ['MONGODB_USERNAME'] \
-        + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] \
-            + ':27017/' + os.environ['MONGODB_DATABASE'] + '?authSource=admin'
-    mongo = MongoClient(mongo_uri)
-    PyMongoModel.update_mongo_client(mongo)
