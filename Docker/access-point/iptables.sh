264e0c6bed6e406bbcc89269f67fd2f7111d7276
Joseph Freeston <snorklerjoe@gmail.com>
2023-03-18 14:30:59 -0400

    Fixed iptables commands

--- Docker/access-point/iptables.sh
+++ Docker/access-point/iptables.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
 # see https://fwhibbit.es/en/automatic-access-point-with-docker-and-raspberry-pi-zero-w
 #iptables-nft -t nat -C POSTROUTING -o ${INTERFACE_API} -j MASQUERADE || iptables-nft -t nat -A POSTROUTING -o ${INTERFACE_API} -j MASQUERADE
-iptables-nft -C FORWARD -i ${INTERFACE_API} -o ${AP_INTERFACE} -m state --state RELATED,ESTABLISHED --dport ${API_PORT} -j ACCEPT || iptables-nft -A FORWARD -i ${INTERFACE_API} -o ${AP_INTERFACE} -m state --state RELATED,ESTABLISHED --dport ${API_PORT} -j ACCEPT
-iptables-nft -C FORWARD -i ${AP_INTERFACE} -o ${INTERFACE_API} --dport ${API_PORT} -j ACCEPT || iptables-nft -A FORWARD -i ${AP_INTERFACE} -o ${INTERFACE_API} --dport ${API_PORT} -j ACCEPT
+iptables-nft -C FORWARD -i ${INTERFACE_API} -o ${AP_INTERFACE} -m state --state RELATED,ESTABLISHED -p TCP --dport ${API_PORT} -j ACCEPT || iptables-nft -A FORWARD -i ${INTERFACE_API} -o ${AP_INTERFACE} -m state --state RELATED,ESTABLISHED -p TCP --dport ${API_PORT} -j ACCEPT
+iptables-nft -C FORWARD -i ${AP_INTERFACE} -o ${INTERFACE_API} -p TCP --dport ${API_PORT} -j ACCEPT || iptables-nft -A FORWARD -i ${AP_INTERFACE} -o ${INTERFACE_API} -p TCP --dport ${API_PORT} -j ACCEPT
