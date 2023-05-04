325052e42675ae1e296f9561e00aea4c2d308b39
Joseph Freeston <snorklerjoe@gmail.com>
2023-05-04 00:10:57 -0400

    avoid newline before protocol

--- src/CubeServer-common/cubeserver_common/models/beaconmessage.py
+++ src/CubeServer-common/cubeserver_common/models/beaconmessage.py
@@ -187,13 +187,13 @@ class BeaconMessage(PyMongoModel):
         """
         if self._id:  # If this came from the database, don't regen
             return self.full_message_bytes_stored
-        return self.prefix + self.line_term.join([
-            b'CSMSG/1.0',
-            self.headers_bytes,
-            self.line_term,
-            self.message_bytes,
-            self.line_term
-        ])
+        return self.prefix + b'CSMSG/1.0' + \
+            self.line_term.join([
+                self.headers_bytes,
+                self.line_term,
+                self.message_bytes,
+                self.line_term
+            ])
     
     @property
     def full_message_bytes_p(self):
